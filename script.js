bd7215466db054f13b8895b450eff7c31c0e4df8
Gabriela Miranda <gabriela.miranda.domingos@gmail.com>
2025-11-24 20:38:43 -0300

    Mudanças na landing page.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+let cardContainer = document.querySelector(".card-container");
 let dados = [];
 
-async function iniciarBusca() {
+async function carregarDados() {
     let resposta = await fetch("data.json");
     dados = await resposta.json();
-    console.log(resposta);
+    // renderizarCards(dados);
+    
+    // Adiciona o ouvinte de evento para o campo de busca
+    const campoBusca = document.getElementById("busca");
+    campoBusca.addEventListener("input", iniciarBusca);
 }
+
+function iniciarBusca(evento) {
+    const termoBusca = evento.target.value.toLowerCase();
+    const resultados = dados.filter(dado => 
+        dado.nome.toLowerCase().includes(termoBusca) || 
+        dado.descricao.toLowerCase().includes(termoBusca)
+    );
+    renderizarCards(resultados);
+}
+
+function renderizarCards(cardsParaRenderizar) {
+    cardContainer.innerHTML = ""; // Limpa os cards existentes
+    for (let dado of cardsParaRenderizar){
+        let article = document.createElement("article");
+        article.classList.add("card");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2> 
+        <p>${dado.data_criacao}</p> 
+        <p>${dado.descricao}.</p> 
+        <p><a href="${dado.link}" target="_blank">Saiba mais</a></p>
+        `
+
+        cardContainer.appendChild(article);
+    }
+}
+
+carregarDados();
